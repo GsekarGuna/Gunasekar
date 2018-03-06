84c9d906dee7909f9a35da69df379145b2db56af
GsekarGuna <36332853+GsekarGuna@users.noreply.github.com>
2018-03-06 19:52:14 +0530

    find print fibonacci series of N numbers

---
+++ Set 6-60.py
@@ -0,0 +1,13 @@
+a= int(input("\nPlease Enter the  Number: "))
+i = 0
+First_Value = 0
+Second_Value = 1
+while(i < a):
+           if(i <= 1):
+                      Next = i
+           else:
+                      Next = First_Value + Second_Value
+                      First_Value = Second_Value
+                      Second_Value = Next
+           print(Next)
+           i = i + 1
