e5358a713b25029cef28989419ced52617d268f0
GsekarGuna <36332853+GsekarGuna@users.noreply.github.com>
2018-05-21 13:47:52 +0530

    Given a string S check if it is a palindrome

--- Set 8-71.py
+++ Set 8-71.py
@@ -1,11 +1,11 @@
-a=int(input("Enter number:"))
-temp=a
-rev=0
-while(a>0):
-    dig=a%10
-    rev=rev*10+dig
-    a=a//10
-if(temp==rev):
-    print("Yes!")
+s=input("Enter The string:")
+def reverse(s):
+  str = ""
+  for i in s:
+    str = i + str
+  return str
+b=reverse(s)
+if s==b:
+  print("Yes")
 else:
-    print("No!")
+  print("No")
